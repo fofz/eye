90d421d0c413cae0680eb6224e5a160e90999d47
fofz <fofz1776@gmail.com>
2020-05-25 05:07:33 -0500

    Add to demo script

--- demo.sh
+++ demo.sh
@@ -1,8 +1,10 @@
 make; 
-bin/palette -n 256 > a.data; 
-bin/julia -r 160x160 -s 3x3 -t -1.5+-1.5i -n 256 -b 256 -c -0.757730701301672+0.121160627920417i | bin/normalize -r 160x160 | bin/dtou16 -r 160x160 | bin/dither -r 160x160 >> a.data;
+mkdir data;
+bin/palette -n 256 > data/palette.data; 
+bin/julia -r 320x320 -s 3x3 -t -1.5+-1.5i -n 256 -b 256 -c -0.757730701301672+0.121160627920417i | bin/normalize -r 320x320 | bin/dtou16 -r 320x320 | bin/dither -r 320x320 > data/julia.data;
+mkdir frames;
 for i in {0..127}; 
-do (echo "P6 160 160 255" > $(printf "%03d" $i).ppm; bin/graph -r 160x160 -n 256 -s $((i * 2)) < a.data >> $(printf "%03d" $i).ppm); 
+do (echo "P6 320 320 255" > frames/$(printf "%03d" $i).ppm; cat data/palette.data data/julia.data | bin/graph -r 320x320 -n 256 -s $((i * 2)) >> frames/$(printf "%03d" $i).ppm); 
 done; 
-montage *.ppm -tile 16x8 -geometry 80x80+0+0 banner.jpg; 
-rm a.data *.ppm
+montage frames/*.ppm -tile 16x8 -geometry 80x80+0+0 banner.jpg;
+convert -delay 4 frames/*.ppm animation.gif;
