4e2d5f99b559ef77d5eec0156c89c2d0ccd75740
fofz <fofz1776@gmail.com>
2020-05-26 19:24:21 -0500

    Add Lenna image and demo

---
+++ demo/lenna.sh
@@ -0,0 +1,5 @@
+mkdir frames;
+for i in {0..31}; 
+do (echo "P6 240 240 255" > frames/$(printf "%03d" $i).ppm; cat palette.data res/image/lenna.data | bin/graph -r 240x240 -n 256 -s $((i * 8)) >> frames/$(printf "%03d" $i).ppm); 
+done; 
+montage frames/*.ppm -tile 8x4 -geometry 160x160+0+0 banner.jpg;
